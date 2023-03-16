5bd71fb8b904fc8cb40fe3c16ffeecbfd2c137dd
Ballard3rd <leonard.ballard.third@gmail.com>
2023-03-16 16:17:56 -0400

    changed from waitCommand to CommandBase

--- src/main/java/frc/robot/commands/GrabberOpen.java
+++ src/main/java/frc/robot/commands/GrabberOpen.java
@@ -5,11 +5,29 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.GrabberSubsystem;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/** A command that opens the grabber. */
-public class GrabberOpen extends InstantCommand {
+/**
+ * A simple command that grabs the game piece with the {@link GrabberSubsystem}. Written explicitly for
+ * pedagogical purposes. Actual code should inline a command this simple with {@link
+ * edu.wpi.first.wpilibj2.command.InstantCommand}.
+ */
+public class GrabberOpen extends CommandBase {
+  // The subsystem the command runs on
+  private final GrabberSubsystem m_grabberSubsystem;
+
   public GrabberOpen(GrabberSubsystem subsystem) {
-    super(subsystem::releaseGamePiece, subsystem);
+    m_grabberSubsystem = subsystem;
+    addRequirements(m_grabberSubsystem);
+  }
+
+  @Override
+  public void initialize() {
+    m_grabberSubsystem.releaseGamePiece();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
   }
 }
